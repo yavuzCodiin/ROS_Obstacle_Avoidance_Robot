8a99e33a76edb46ce0a43c39282a087e12a62236
yavuzCodiin <82445309+yavuzCodiin@users.noreply.github.com>
2021-08-09 16:25:22 +0300

    commit+++

--- robot_yavuzClass.py
+++ robot_yavuzClass.py
@@ -27,16 +27,15 @@ class Robot:
         self.rangess     = LaserScan()
         self.min_dist    = 0
         self.speed.linear.x = 0.0
-        self.speed.angular.x = 0.0
-
-
+        self.speed.angular.z = 0.0
+        
         # Node cycle rate (in Hz)
-        self.loop_rate = rospy.Rate(2)
+        self.loop_rate = rospy.Rate(0.6)
      
         #Publishers
         
         #FOR LASER
-        #self.laser_pub = rospy.Publisher('/revised_scan', LaserScan, queue_size = 10)
+        self.laser_pub = rospy.Publisher('/scan', LaserScan, queue_size = 10)
         
          #FOR CAMERA
      #--------------------------
@@ -44,95 +43,85 @@ class Robot:
      #--------------------------    
         
          #FOR SPEED
-        self.cmd_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+        
+
 #------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------        
         #Subscribers
         
          #FOR LASER
-        rospy.Subscriber('/scan', LaserScan, self.callback)    #pub laser scan
-        
+        rospy.Subscriber('/scan', LaserScan, self.scan_callback)
          #FOR CAMERA
-        #rospy.Subscriber(self.image_topic, Image, self.image_callback)
+        #start robot içersinde
         
          #FOR SPEED
-                
-    #def scann_callback(self, msg):
-     #self.rangess = self.msg.ranges
-     #self.min_distance = min(self.rangess)
-     #print(self.min_distance)
+        rospy.Subscriber("/cmd_vel", Twist, self.callback_vel)
 
     #CALLBCACK FOR TWIST() for to subscribe 
     def callback_vel(self,data):
         print(data)
-        #self.loop_rate.sleep()
-
-
-    #Callback function For LASER      
-    def callback(self, msg):
-     current_time=rospy.Time.now()
-     self.rangess.header.stamp=current_time
-     self.rangess.header.frame_id='base_scan'
-     self.rangess.ranges=msg.ranges[0:72]
-     self.rangess.intensities=msg.intensities[0:72]
-     #self.min_dist = min(self.rangess.ranges)
-     self.laser_pub.publish(self.rangess)
-     #print(self.min_dist)
-
+        self.loop_rate.sleep()
+    
+    #CALLBACK FOR LASER() for to subscribe
     def scan_callback(self, msg):
+        
+        #self.laser_pub.publish(self.rangess)
         
         for i in range(len(msg.ranges)):
             self.min_dist = min(msg.ranges)
         
-        print(self.min_dist)    
-
+        #self.loop_rate.sleep()
       
 #------------------------------------------------------------------------------------------         
     #Callback function For CAMERA
     def image_callback(self, msg):
-	    print("Received an image!")
 	    try:
-	 
 	 	   self.image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 	       
 	    except self.cv2.error as e:
 	 	    print(e)
 	 
 	    else:
-	 	   self.foto = cv2.imwrite('camerax_sonimage.jpeg', self.image)
-	    self.loop_rate.sleep()		 	 	
+             self.foto = cv2.imwrite('camerax_sonimage.jpeg', self.image)
+             print("Image Saved!")
+             self.loop_rate.sleep()     
+	    		 	   	
 #------------------------------------------------------------------------------------------		 
 #------------------------------------------------------------------------------------------		 
     def start_robot(self):
-             
-        while not rospy.is_shutdown():
-         self.laser_pub = rospy.Publisher('/revised_scan', LaserScan, queue_size = 10)  
-         rospy.Subscriber("/cmd_vel", Twist, self.callback_vel)  
-         
-         self.speed.linear.x = 0.22
-         self.speed.angular.x = 0.0
-         self.cmd_pub.publish(self.speed)
 
-         
-         rospy.Subscriber('/revised_scan', LaserScan, self.scan_callback)
-         
+        self.laser_pub.publish(self.rangess)
+
+        self.speed.linear.x = 0.11
+        self.speed.angular.z = 0.0
+        self.cmd_pub.publish(self.speed)
+
+        while not rospy.is_shutdown():
+                
+         print(self.min_dist)  
          if(self.min_dist<1):
              self.speed.linear.x  = 0.0
-             self.speed.angular.x = 0.0
+             self.speed.angular.z = 0.0
              self.cmd_pub.publish(self.speed)
              rospy.Subscriber(self.image_topic, Image, self.image_callback)    
          
          else:
-             print("fuuuu")
+             print("continue")
              self.speed.linear.x  = 0.22
-             self.speed.angular.x = 0.0
+             self.speed.angular.z = 0.0
              self.cmd_pub.publish(self.speed)
-             #self.loop_rate.sleep() 
+         
+         self.loop_rate.sleep()
+              
        
 if __name__ == '__main__':
+    rospy.init_node('Robot', anonymous=True)		 
      
-     rospy.init_node('Robot', anonymous=True)		 
-     
-     robot_node = Robot()
-     robot_node.start_robot() 
-		 
+    robot_node = Robot()
+    robot_node.start_robot()
+    
+    
+    
+    
